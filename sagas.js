f60443b628ffddddabdb389f1a06541775bacd5f
pywd <yuki.wd7@gmail.com>
2019-05-06 18:57:05 +0900

    sagaチュートリアル終わり

---
+++ sagas.js
@@ -0,0 +1,23 @@
+import { put, takeEvery, all, call } from 'redux-saga/effects';
+
+export const delay = ms => new Promise(res => setTimeout(res, ms));
+
+export function* helloSaga() {
+  console.log('Hello Sagas!');
+}
+
+// Our worker Saga: will perform the async increment task
+export function* incrementAsync() {
+  // use the call Effect
+  yield call(delay, 1000);
+  yield put({ type: 'INCREMENT' });
+}
+
+// Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
+export function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync);
+}
+
+export default function* rootSaga() {
+  yield all([helloSaga(), watchIncrementAsync()]);
+}
